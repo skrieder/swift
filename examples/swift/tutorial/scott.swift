f00b638a3f79d8acca33bbca7e8895f1665f0f9f
Scott J. Krieder <skrieder@skrieder-desktop.(none)>
2012-05-25 12:34:46 -0500

    working in ubunut with proper pathing

--- examples/swift/tutorial/scott.swift
+++ examples/swift/tutorial/scott.swift
@@ -1,7 +1,7 @@
 type messagefile;
 
 app (messagefile t) greeting() { 
-   sh "/Users/scottkrieder/swift/examples/swift/tutorial/scott.sh" stdout=@filename(t);
+   sh "/home/skrieder/swift/examples/swift/tutorial/scott.sh" stdout=@filename(t);
 }
 
 messagefile outfile <"scott.txt">;

72aa0180f4c11f106d84ac5e3d2b8fbd166c50f0
Scott J. Krieder <skrieder@iit.edu>
2012-06-08 12:52:01 -0500

    working now, friday am

--- multi/3inloop.swift
+++ multi/3inloop.swift
@@ -1,4 +1,3 @@
-
 type messagefile; 
 
 app (messagefile t) countwords (messagefile f) {   
@@ -10,6 +9,7 @@ string outputNames = "a1sm1ms b1sm1ms c1sm1ms";
 messagefile outputfiles[] <fixed_array_mapper;files=outputNames>;
 
 foreach f,i in outputfiles {
-  messagefile c <single_file_mapper; file=@strcat(i, ".result")>;
+  //messagefile c <single_file_mapper; file=@strcat(i, ".result")>;
+  messagefile c;
   c = countwords(f);
 }
